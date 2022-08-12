91a049fecd0e8ce587dab5cec19f429e34bd8ff6
James Murphy <jamesivdeveloper@gmail.com>
2022-08-11 22:14:11 -0500

    add notes to task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,30 @@
 var formEl = document.querySelector("#create-new-task-form");
 var onDeckEl = document.querySelector(".on-deck-list");
 
-/* create task function */
+// create task function
 
 var createTask = function (event) {
   event.preventDefault();
+  var taskInput = document.querySelector(".new-task").value;
+  var notesInput = document.querySelector(".notes").value;
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = document.querySelector(".new-task").value;
 
+  var taskEl = document.createElement("div");
+  taskEl.className = "task";
+
+  taskEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskInput +
+    "</h3><span class='task-notes'>" +
+    notesInput +
+    "</span>";
+
+  listItemEl.appendChild(taskEl);
   onDeckEl.appendChild(listItemEl);
 };
 
-/* add task */
+// add task
 
 formEl.addEventListener("submit", createTask);
