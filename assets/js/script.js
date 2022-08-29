158412a60563d32c08b9977345395447c5efa507
James Murphy <jamesivdeveloper@gmail.com>
2022-08-29 10:46:27 -0500

    change task location based on task status

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,148 @@
 var formEl = document.querySelector("#create-new-task-form");
 var onDeckEl = document.querySelector(".on-deck-list");
+var inProgressEl = document.querySelector(".in-progress");
+var doneEl = document.querySelector(".done");
+var pageContentEl = document.querySelector(".page-content");
 
-// create task function
+var taskIdCounter = 0;
 
-var createTask = function (event) {
+var tasks = [];
+
+// handle form submit
+
+var taskFormHandler = function (event) {
   event.preventDefault();
   var taskInput = document.querySelector(".new-task").value;
   var notesInput = document.querySelector(".notes").value;
 
+  if (!taskInput) {
+    alert("You must provide a task");
+    return false;
+  }
+
+  document.querySelector(".new-task").value = "";
+  document.querySelector(".notes").value = "";
+
+  var isEdit = formEl.hasAttribute("data-task-id");
+
+  if (isEdit) {
+    var taskId = formEl.getAttribute("data-task-id");
+    completeEditTask(taskInput, notesInput, taskId);
+  } else {
+    var taskDataObj = {
+      task: taskInput,
+      notes: notesInput,
+      status: "on deck",
+    };
+  }
+
+  createTask(taskDataObj);
+};
+
+// create task function
+
+var createTask = function (taskDataObj) {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
+  listItemEl.setAttribute("data-task-id", taskIdCounter);
 
   var taskEl = document.createElement("div");
   taskEl.className = "task";
 
   taskEl.innerHTML =
     "<h3 class='task-name'>" +
-    taskInput +
+    taskDataObj.task +
     "</h3><span class='task-notes'>" +
-    notesInput +
+    taskDataObj.notes +
     "</span>";
 
   listItemEl.appendChild(taskEl);
-  onDeckEl.appendChild(listItemEl);
+
+  var taskActionsEl = createTaskActions(taskIdCounter);
+  listItemEl.appendChild(taskActionsEl);
+
+  switch (taskDataObj.status) {
+    case "on deck":
+      taskActionsEl.querySelector(
+        "select[name='status-change']"
+      ).selectedIndex = 0;
+      onDeckEl.append(listItemEl);
+      break;
+    case "in progress":
+      taskActionsEl.querySelector(
+        "select[name='status-change']"
+      ).selectedIndex = 1;
+      inProgressEl.append(listItemEl);
+      break;
+    case "done":
+      taskActionsEl.querySelector(
+        "select[name='status-change']"
+      ).selectedIndex = 2;
+      doneEl.append(listItemEl);
+      break;
+    default:
+      console.log("Something went wrong!");
+  }
+
+  taskDataObj.id = taskIdCounter;
+
+  tasks.push(taskDataObj);
+
+  taskIdCounter++;
+
+  // onDeckEl.appendChild(listItemEl);
+};
+
+// create task actions
+
+var createTaskActions = function (taskId) {
+  var actionaContainerEl = document.createElement("div");
+  actionaContainerEl.className = "task-actions";
+
+  // change status dropdown
+  var statusSelectEl = document.createElement("select");
+  statusSelectEl.setAttribute("name", "status-change");
+  statusSelectEl.setAttribute("data-task-id", taskId);
+  statusSelectEl.className = "select-status";
+  actionaContainerEl.appendChild(statusSelectEl);
+  // create status options
+  var statusChoices = ["On Deck", "In Progress", "Done"];
+
+  for (var i = 0; i < statusChoices.length; i++) {
+    var statusOptionEl = document.createElement("option");
+    statusOptionEl.setAttribute("value", statusChoices[i]);
+    statusOptionEl.textContent = statusChoices[i];
+
+    statusSelectEl.appendChild(statusOptionEl);
+  }
+
+  return actionaContainerEl;
+};
+
+// handle task status change
+
+var taskStatusChangeHandler = function (event) {
+  var taskId = event.target.getAttribute("data-task-id");
+
+  var statusValue = event.target.value.toLowerCase();
+
+  var taskSelected = document.querySelector(
+    ".task-item[data-task-id='" + taskId + "']"
+  );
+
+  if (statusValue === "on deck") {
+    onDeckEl.appendChild(taskSelected);
+  } else if (statusValue === "in progress") {
+    inProgressEl.appendChild(taskSelected);
+  } else if (statusValue === "done") {
+    doneEl.appendChild(taskSelected);
+  }
 };
 
 // add task
 
-formEl.addEventListener("submit", createTask);
+formEl.addEventListener("submit", taskFormHandler);
+
+// change status
+
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
